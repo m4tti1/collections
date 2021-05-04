015a7c48fe910be2cd8b52bc9c069d3db8964163
matti <matti.wagner@code.berlin>
2021-05-04 03:50:05 +0200

    cleaning code and added pys

--- collectionsapp/website.py
+++ collectionsapp/website.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
@@ -11,6 +11,10 @@ app.config.from_pyfile("config.py")
 def index():
     return render_template('index.html', page_title="collections")
 
+#@app.route('/item', methods=['POST'])
+#def safe_item_data():
+#    if request.method == 'POST':
+#        user_submitted_data = 
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
